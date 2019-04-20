824e9f9b2e4a9b102d44e200fbc33fd863d5ecec
Administrator <1037438704@qq.com>
2019-04-20 08:47:14 +0800

    Initial commit

---
+++ app/src/main/java/com/ninetaildemonfox/zdl/ninetaildemonfox/MainActivity.java
@@ -0,0 +1,19 @@
+package com.ninetaildemonfox.zdl.ninetaildemonfox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author NineTailDemonFox
+ * @date 2019/4/20 8:23
+ * 功能描述：主界面
+ * 联系方式：1037438704@qq.com
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
