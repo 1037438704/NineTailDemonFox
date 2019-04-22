51168f1692bf6c6d60b042f41974f60043b644f4
Administrator <1037438704@qq.com>
2019-04-22 09:52:02 +0800

    继续调试

--- ninetails/src/main/java/com/ninetaildemonfox/zdl/ninetails/MainActivity.java
+++ ninetails/src/main/java/com/ninetaildemonfox/zdl/ninetails/MainActivity.java
@@ -1,13 +1,4 @@
 package com.ninetaildemonfox.zdl.ninetails;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+public class MainActivity {
 }
